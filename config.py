b323c94cd84c06fac45f64e24fcd05e9682ea94a
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-18 18:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Telegram Bot Token (get from BotFather)
-TELEGRAM_BOT_TOKEN = "your_bot_token_here"
+TELEGRAM_BOT_TOKEN = "7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs"
 
 # Pyrogram API credentials (get from my.telegram.org)
-API_ID = your_api_id
-API_HASH = "your_api_hash_here"
+API_ID = 27461953
+API_HASH = "8a19a6a007044ff7b41ada4b377cdfba"
 
 # MongoDB URI (e.g., "mongodb://localhost:27017" or cloud URI)
-MONGO_URI = "your_mongodb_uri_here"
+MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
